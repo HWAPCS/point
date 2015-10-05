fc178bde162fa24606b3e8164aa1011b36177e1b
Jonathan Damico <jj_damico@yahoo.com>
2015-10-05 16:36:50 -0700

    Update MasterTester.java

--- src/MasterTester.java
+++ src/MasterTester.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class MasterTester {
-	public static void main (String args[]) {
+	public static void main (String args[]) throws Exception {
 		Scanner s = new Scanner (System.in);
 		Point firstPoint = new Point (0,1);
 		Point secondPoint = new Point (0,0);
