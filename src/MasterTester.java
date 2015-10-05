a43c84ab564d4d267b15b3d6bde152bd621766cb
Jonathan Damico <jj_damico@yahoo.com>
2015-10-05 12:41:31 -0700

    fix error

--- src/MasterTester.java
+++ src/MasterTester.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class PointTester {
+public class MasterTester {
 	public static void main (String args[]) {
 		Scanner s = new Scanner (System.in);
 		Point firstPoint = new Point (0,1);
