f7cb050d4f4160225a02020a184584182c93267b
Jonathan Damico <jj_damico@yahoo.com>
2015-10-05 12:31:17 -0700

    add build instructions and a master class

---
+++ src/MasterTester.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class PointTester {
+	public static void main (String args[]) {
+		Scanner s = new Scanner (System.in);
+		Point firstPoint = new Point (0,1);
+		Point secondPoint = new Point (0,0);
+		System.out.println(firstPoint.distance(secondPoint));
+	}
+}
