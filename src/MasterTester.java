9ae3a45a945079b5508e830f7dfb1c7329b68780
Jonathan Damico <jj_damico@yahoo.com>
2015-10-05 16:29:02 -0700

    Update MasterTester.java

--- src/MasterTester.java
+++ src/MasterTester.java
@@ -5,6 +5,7 @@ public class MasterTester {
 		Scanner s = new Scanner (System.in);
 		Point firstPoint = new Point (0,1);
 		Point secondPoint = new Point (0,0);
-		System.out.println(firstPoint.distance(secondPoint));
+		if(firstPoint.distance(secondPoint)!=1)
+			throw new IncorrectlyCompletedException();
 	}
 }
