cea6bdd07224faeea433ef37233da9335e054556
Jonathan Damico <jj_damico@yahoo.com>
2015-10-06 11:43:25 -0700

    update master tester to reflect changes in point class

--- src/MasterTester.java
+++ src/MasterTester.java
@@ -7,7 +7,7 @@ public class MasterTester {
 		Scanner s = new Scanner (System.in);
 		Point firstPoint = new Point (0,1);
 		Point secondPoint = new Point (0,0);
-		if(firstPoint.distance(secondPoint)!=1)
+		if(firstPoint.getDistance(secondPoint)!=1)
 			throw new IncorrectlyCompletedException();
 	}
 }
